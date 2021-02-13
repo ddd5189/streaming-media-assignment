6751daccbfcd65f909a207a9c34cb8bfcca68536
Drew Donovan <ddd5189@rit.edu>
2021-02-12 22:03:54 -0500

    fixed error

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -4,7 +4,6 @@ const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
-
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
@@ -12,17 +11,16 @@ const getIndex = (request, response) => {
 };
 
 const getPage2 = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(page2);
-    response.end();
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page2);
+  response.end();
 };
 
 const getPage3 = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(page3);
-    response.end();
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page3);
+  response.end();
 };
-
 
 module.exports.getIndex = getIndex;
 module.exports.getPage2 = getPage2;
